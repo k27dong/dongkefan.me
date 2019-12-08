d5a447a3091fde61634f5f13a3d892d10d4e8d0e
Kevin Dong <k27dong@edu.uwaterloo.ca>
2019-12-08 18:40:52 -0500

    added skip button for typing animation

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import styled from "styled-components"
 import Fade from "react-reveal/Fade"
-import { Divider } from "antd"
+import { Divider, Button } from "antd"
 
 import Intro from "./sections/intro"
 import Timeline from "./sections/timeline"
@@ -15,12 +15,34 @@ const Container = styled.div`
   }
 `
 
+const SkipButton = styled(Button)`
+  float: right;
+`
+
+const isMobile = () => {
+  return (
+    typeof window.orientation !== "undefined" ||
+    navigator.userAgent.indexOf("IEMobile") !== -1
+  )
+}
+
 const App = () => {
   const [typeDone, setTypeDone] = useState(false)
+  const [skipFlag, setSkipFlag] = useState(false)
 
   return (
     <Container>
-      <Intro onTypistDone={e => setTypeDone(true)} />
+      {!typeDone && (
+        <SkipButton
+          onClick={e => {
+            setTypeDone(true)
+            setSkipFlag(true)
+          }}
+        >
+          {isMobile() ? "Skip" : "Skip Animation"}
+        </SkipButton>
+      )}
+      <Intro type={skipFlag} onTypistDone={e => setTypeDone(true)} />
       {typeDone && (
         <Fade>
           <Timeline />
--- src/app/sections/intro.js
+++ src/app/sections/intro.js
@@ -6,6 +6,7 @@ import _ from "lodash"
 import { getAge } from "../util/helpers"
 
 const IntroWrapper = styled.div`
+  margin-top: 20px;
   text-align: left;
   line-height: 1.25;
   color: rgba(255, 255, 255, 0.65);
@@ -19,22 +20,12 @@ const IntroWrapper = styled.div`
 const finishDelay = 400
 const beginDelay = 300
 
-const Intro = ({ onTypistDone = _.noop }) => (
-  <IntroWrapper>
-    <Typist
-      startDelay={1500}
-      avgTypingDelay={45}
-      cursor={{ hideWhenDone: true }}
-      onTypingDone={onTypistDone}
-    >
-      <span>Hello,</span>
-
-      <Typist.Delay ms={finishDelay} />
-      <br />
-      <Typist.Delay ms={beginDelay} />
+const Intro = ({ onTypistDone = _.noop, type }) =>
+  type ? (
+    <IntroWrapper>
+      <span>Hello,</span> <br />
       <span>
         My name is Kefan Dong.
-        <Typist.Delay ms={300} />
         <a
           style={{ color: "dodgerblue" }}
           target="_blank"
@@ -43,11 +34,8 @@ const Intro = ({ onTypistDone = _.noop }) => (
           {" "}
           (k27dong)
         </a>
-      </span>
-
-      <Typist.Delay ms={finishDelay} />
+      </span>{" "}
       <br />
-      <Typist.Delay ms={beginDelay} />
       <span>
         Studying Electrical Engineering at the{" "}
         <a
@@ -57,22 +45,70 @@ const Intro = ({ onTypistDone = _.noop }) => (
         >
           University of Waterloo.
         </a>
-      </span>
-
-      <Typist.Delay ms={finishDelay} />
+      </span>{" "}
       <br />
-      <Typist.Delay ms={beginDelay} />
-      <span>I am {getAge()} years old. Living in Mississauga, ON. Canada.</span>
-
-      <Typist.Delay ms={finishDelay} />
-      <br />
-      <Typist.Delay ms={200} />
-      <br />
-      <Typist.Delay ms={beginDelay + 200} />
+      <span>
+        I am {getAge()} years old. Living in Mississauga, ON. Canada.
+      </span>{" "}
+      <br /> <br />
       <span>Experiences:</span>
-      <Typist.Delay ms={1000} />
-    </Typist>
-  </IntroWrapper>
-)
+    </IntroWrapper>
+  ) : (
+    <IntroWrapper>
+      <Typist
+        startDelay={1500}
+        avgTypingDelay={45}
+        cursor={{ hideWhenDone: true }}
+        onTypingDone={onTypistDone}
+      >
+        <span>Hello,</span>
+
+        <Typist.Delay ms={finishDelay} />
+        <br />
+        <Typist.Delay ms={beginDelay} />
+        <span>
+          My name is Kefan Dong.
+          <Typist.Delay ms={300} />
+          <a
+            style={{ color: "dodgerblue" }}
+            target="_blank"
+            href="https://github.com/k27dong"
+          >
+            {" "}
+            (k27dong)
+          </a>
+        </span>
+
+        <Typist.Delay ms={finishDelay} />
+        <br />
+        <Typist.Delay ms={beginDelay} />
+        <span>
+          Studying Electrical Engineering at the{" "}
+          <a
+            style={{ color: "#FFD54F" }}
+            href="https://uwaterloo.ca/future-students/programs/electrical-engineering"
+            target="_blank"
+          >
+            University of Waterloo.
+          </a>
+        </span>
+
+        <Typist.Delay ms={finishDelay} />
+        <br />
+        <Typist.Delay ms={beginDelay} />
+        <span>
+          I am {getAge()} years old. Living in Mississauga, ON. Canada.
+        </span>
+
+        <Typist.Delay ms={finishDelay} />
+        <br />
+        <Typist.Delay ms={200} />
+        <br />
+        <Typist.Delay ms={beginDelay + 200} />
+        <span>Experiences:</span>
+        <Typist.Delay ms={1000} />
+      </Typist>
+    </IntroWrapper>
+  )
 
 export default Intro
